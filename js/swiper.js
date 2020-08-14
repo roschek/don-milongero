17b14370d4928a054a7f3e6345b0a0a6d5116446
Alexey Roschektaev <55943475+roschek@users.noreply.github.com>
2020-08-14 17:10:14 +0300

    Add files via upload
    добавил еще два слайдера, сверстал блоки под разрешение 1440

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,9 @@
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
-    centeredSlides: true,
+    centeredSlides: false,
     slidesPerView: 'auto',
+
     coverflowEffect: {
         rotate: 50,
         stretch: 0,
@@ -12,5 +13,21 @@ var swiper = new Swiper('.swiper-container', {
     },
     pagination: {
         el: '.swiper-pagination',
+    },
+});
+
+var swiper2 = new Swiper('.swiper-container-s2', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 });
